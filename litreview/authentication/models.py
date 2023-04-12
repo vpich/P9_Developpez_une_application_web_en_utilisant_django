83ae9323a1426cd6961544d87431eaf1723d2f70
Virginie PICH <red_iv@hotmail.fr>
2023-04-12 17:39:32 +0200

    Modification du modèle User pour qu'il n'y ait pas de champs email

--- litreview/authentication/models.py
+++ litreview/authentication/models.py
@@ -1,6 +1,6 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractBaseUser
 
 
-class User(AbstractUser):
-    pass
+class User(AbstractBaseUser):
+    username = models.CharField(max_length=150)
